692e52cb8839ec7df43cfe44bb3f2a8f7300d3b2
redmundnacario <redmund.nacario@gmail.com>
2021-09-28 15:21:49 +0800

    adds getAverageCost mongoose middleware when updating or deleting courses

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,36 @@ const CourseSchema = new mongoose.Schema({
     },
 });
 
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: bootcampId,
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageCost: { $avg: "$tuition" },
+            },
+        },
+    ]);
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Call a method getAverageCost after save
+CourseSchema.post("save", async function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call a method getAverageCost before remove
+CourseSchema.pre("remove", async function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
--- tests/courses.routes.test.js
+++ tests/courses.routes.test.js
@@ -1,1 +1,9 @@
-//check if fields exist in update or PUT or PATCH method
+// General
+// should test all routes
+// Should test the calculation of averageCost in Bootcamp after creating and removing course
+// should separate tests from model and routes
+
+//Bootcamp
+// should test id Courses reverse populate in Bootcamp get all route
+// should test the geocoder.geocode in Bootcamp routes
+// should test  cascade deletion of courses when as bootcamp is deleted
